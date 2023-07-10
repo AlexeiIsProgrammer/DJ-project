103000a686dfdbe34e4a9c1cc6cf79b4457048fd
Alexei <avenge.now.r@mail.ru>
2023-07-10 17:48:39 +0300

    feat: add build

--- src/localization.js
+++ src/localization.js
@@ -1,18 +1,20 @@
-import localization from '../languages.json'
+import localization from './languages.json'
 
 window.addEventListener('DOMContentLoaded', () => {
     const languages = document.querySelector('.languages')
     const languagesItems = languages.querySelectorAll('.languages__radio')
     let currentLang = 'eng'
 
-    if (localStorage.getItem('language')) {
-        updateLanguage(localStorage.getItem('language'))
+    if (localStorage.getItem('site-language')) {
+        const storageValue = localStorage.getItem('site-language')
+        updateLanguage(storageValue)
+        languages.querySelector(`.languages__radio[value="${storageValue}"]`).checked = true
     } else {
         updateLanguage(currentLang)
     }
 
     window.addEventListener('beforeunload', function () {
-        localStorage.setItem('language', currentLang)
+        localStorage.setItem('site-language', currentLang)
     })
 
     languagesItems.forEach(el => el.addEventListener('change', function (e) {
